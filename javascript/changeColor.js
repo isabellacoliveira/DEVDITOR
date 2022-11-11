0a6f4d70c2d56275d1e76e3011542c9b58815085
Isabella Cruz de Oliveira <isaaholiveira15@gmail.com>
2022-11-11 14:40:03 -0300

    realizando a task 688: Organizar a responsividade da página

--- javascript/changeColor.js
+++
@@ -1,6 +0,0 @@
-const mudaCor = document.getElementById('b1')
-const mudaFundo = document.getElementById('caixa')
-
-mudaCor.addEventListener("input", () => {
-    mudaFundo.style.backgroundColor = mudaCor.value;
-})
--- javascript/salvandoProjeto.js
+++ javascript/salvandoProjeto.js
@@ -1,127 +1,104 @@
-// fazer uma função que pegue os textos adicionados nos dois inputs e o codigo inserido no editor (ok-)
-//  a cor que está a caixa de texto (ok)
-// a linguagem e associe essa ação ao botão salvar projeto (ok)
-// ao ser salvo, tudo isso deve ser adicionado a página de comunidade 
-// lá deve aparecer o card com a cor, o nome do projeto e a descrição 
+const preservar = document.querySelector(".preservar");
+const botaoSalvar = document.querySelector("#b2");
+const tituloDoProjeto = document.querySelector("#text1");
+const descricaoDoProjeto = document.querySelector("#text2");
+const corDoProjeto = document.querySelector("#b1");
+const pegaLinguagem = document.querySelector(".linguagem");
+const areaCdg = document.querySelector("#parteCode");
 
-// função 1 para a cor 
-const preservar = document.querySelector('.preservar')
-const botaoSalvar = document.querySelector('#b2')
-const tituloDoProjeto = document.querySelector('#text1')
-const descricaoDoProjeto = document.querySelector('#text2')
-const corDoProjeto = document.querySelector('#b1')
-const pegaLinguagem = document.querySelector('.linguagem')
-const areaCdg  = document.querySelector('#parteCode')
+const mudaCor = document.getElementById('b1')
+const mudaFundo = document.getElementById('caixa')
+const bordaDoCard = document.getElementById('bordaDoCard') 
 
-corDoProjeto.addEventListener('input', function(evento) {
-    let corzinha = evento.target.value
-    preservar.style = `background: ${corzinha}`;
-})
+corDoProjeto.addEventListener("input", function (evento) {
+	let corzinha = evento.target.value;
+	preservar.style = `background: ${corzinha}`;
+}); 
+
+// preservar.addEventListener("change", (evento) => {
+//     let mudaraCor = evento.target.value; 
+//     bordaDoCard.style = `background: ${mudaraCor}`
+// })
+
 
 function criarProjeto() {
-    var projetinho = {
-        'id': atribuiID(), // vou criar 
-        'detalhesDoProjeto': {
-            'nome': tituloDoProjeto.value,
-            'descricao': descricaoDoProjeto.value, 
-            'cor': corDoProjeto.value, 
-            'language': pegaLinguagem.value, 
-            'code': areaCdg.querySelector('code').innerText
-        }
-    
+	var projetinho = {
+		// id: atribuiID(), // vou criar
+		detalhesDoProjeto: {
+			nome: tituloDoProjeto.value,
+			descricao: descricaoDoProjeto.value,
+			cor: corDoProjeto.style.backgroundcolor,
+			language: pegaLinguagem.value,
+			code: areaCdg.querySelector("code").innerText,
+		},
+	};
+
+    let corDoCard = document.querySelector(".preservar").style.backgroundColor;
+    !corDoCard ? corDoCard = '#6BD1FF' : null 
+    let codee = document.querySelector('#parteCode').innerText; 
+
+    let lingua = document.querySelector('.linguagem').value
+
+	console.log("projetinho", projetinho);
+
+        $("#cardsCriados").append(`<div class="cardizins">  
+    <div id="bordaDoCard" style="background: ${corDoCard};">
+        <div class="barraSuperior">
+            <div id="vermelho">
+            <img src="../imagens/vermelho.png" alt="">
+            </div>
+            <div id="amarelo">
+                <img src="../imagens/amarelo.png" alt="">
+            </div>
+            <div id="verde">
+                <img src="../imagens/verde.png" alt="">
+            </div>
+        </div id="recebeConteudo">
+            <code class="preview-hljs ${pegaLinguagem.value} hljs language-${lingua}" contenteditable="true" aria-label="Editor de código">${codee}</code>
+        </div>
+        
+        <div onmouseover="mOver(this)" onmouseout="mOut(this)" id="descritivo">
+            <div id="descricaoProjeto" >
+                <h1>${projetinho.detalhesDoProjeto.nome}<h1>
+                <p>${projetinho.detalhesDoProjeto.descricao}<p> 
+            </div>
+            <img src="../imagens/comentario.png" id="comentario">
+            <img src="../imagens/coracao.png" id="coracao">
+    </div>`);
+
+    if (projetinho.detalhesDoProjeto.nome == '') {
+        alert('Informe o nome do projeto.')
+        return false
+    } else if (projetinho.detalhesDoProjeto.descricao == '') {
+        alert('Por favor descreva o seu projeto.')
+        return false
+     } else if (codee == '') {
+          alert('Desculpe, não encontramos código no seu projeto.')
+          return false
     }
-   return projetinho;
 }
 
-botaoSalvar.addEventListener('click', function (evento) {
-        const novoProjeto = criarProjeto() 
-        console.log(novoProjeto)
-})
+botaoSalvar.addEventListener("click", function (evento) {
+	 criarProjeto();
+     console.log(clicks);
+});
 
-function atribuiID() {
-        return atribuiID
-}
+var clicks = 0;
+console.log(clicks);
 
-var clicks = 0 
-console.log(clicks)  
-
-function criarContainer() {
-    
-    return (` <div class="cardizins">  
-  
-<div class="preservar">
-    <div class="barraSuperior">
-        <div id="vermelho">
-           <img src="../imagens/vermelho.png" alt="">
-        </div>
-        <div id="amarelo">
-            <img src="../imagens/amarelo.png" alt="">
-        </div>
-        <div id="verde">
-            <img src="../imagens/verde.png" alt="">
-        </div>
-    </div> 
-        <code class="preview-hljs" contenteditable="true" aria-label="Editor de código" >socorro</code>
-    </div>
-       
-    <div onmouseover="mOver(this)" onmouseout="mOut(this)" id="descritivo">
-        <div id="descricaoProjeto" >
-    
-            
-        </div>
-        <img src="../imagens/comentario.png" id="comentario">
-        <img src="../imagens/coracao.png" id="coracao">
-</div>`)
-   
-}
-
-criarContainer()
-
-function criaDiv() {
-    return (`<div id="recebeCard"> 
-        <h1>teste</h1>     
-    </div>`)
-}
-
-const botaoVaiParaComunidade = document.querySelector("#comunidadeBot")
-const divLimpa = document.querySelector('.caixaDeTexto')
-const recebeCard = document.querySelector('#cardsCriados')
+const botaoVaiParaComunidade = document.querySelector("#comunidadeBot");
+const botaoVaiParaEditor = document.querySelector("#editorBot");
+const divLimpa = document.querySelector(".caixaDeTexto");
+const recebeCard = document.querySelector("#cardsCriados");
 
 botaoVaiParaComunidade.addEventListener("click", (evento) => {
-    document.querySelector('.caixaDeTexto').style.display = 'none';
-    document.querySelector('#contemCards').style.display = 'block';
-}) 
+	document.querySelector(".caixaDeTexto").style.display = "none";
+	document.querySelector("#contemCards").style.display = "block";
+});
 
+botaoVaiParaEditor.addEventListener("click", (evento) => {
+	document.querySelector(".caixaDeTexto").style.display = "block";
+	document.querySelector("#contemCards").style.display = "none";
+});
 
-    botaoSalvar.addEventListener("click", (evento) => {
-            recebeCard.innerHTML += criarContainer()
-            console.log(clicks) 
-    })
-
-
-
-
-
-
-
-
-
-// função com os parametros dos valores que pegou += 
-// associar ao click do botão 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+{/* <textarea id="conteudoInserido" class="preview-hljs ${lingua}"> ${codee}</textarea> */}
